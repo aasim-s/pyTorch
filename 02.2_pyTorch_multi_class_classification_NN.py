6c223f5bb271b94c1a9a6854bf4c4673ef5ffedb
aasim-s <aasimsayyed214@gmail.com>
2024-10-10 00:51:27 +0530

    multi class slassification

--- 02.2_pyTorch_multi_class_classification_NN.py
+++ 02.2_pyTorch_multi_class_classification_NN.py
@@ -1,1 +1,123 @@
 import torch
+from torch import nn
+from torchmetrics import Accuracy
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from helper_functions import plot_decision_boundary
+
+NUM_CLASSES = 4
+NUM_FEATURES = 2
+RANDOM_SEED = 42
+
+# create multi class data
+X_blob, y_blob = make_blobs(n_samples=1000,
+                            n_features=NUM_FEATURES,
+                            centers=NUM_CLASSES,
+                            cluster_std=1.5,
+                            random_state=RANDOM_SEED
+                            )
+
+X_blob = torch.from_numpy(X_blob).type(torch.float)
+y_blob = torch.from_numpy(y_blob).type(torch.LongTensor)
+
+X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
+                                                                        y_blob,
+                                                                        test_size=0.2,
+                                                                        random_state=RANDOM_SEED
+                                                                        )
+print(X_blob_train.shape)
+print(y_blob_train.shape)
+print(X_blob_test.shape)
+print(y_blob_test.shape)
+
+plt.figure(figsize=(10, 7))
+plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
+# plt.show()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_features),
+        )
+
+    def forward(self, x):
+        return self.linear_layer_stack(x)
+
+
+model_4 = BlobModel(input_features=NUM_FEATURES,
+                    output_features=NUM_CLASSES, hidden_units=8).to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model_4.parameters(), lr=0.1)
+
+
+# def accuracy_fn(y_true, y_pred):
+#    correct = torch.eq(y_true, y_pred).sum().item()
+#    acc = (correct / len(y_pred)) * 100
+#   return acc
+torchmetrics_accuracy = Accuracy(task="multiclass", num_classes=4).to(device)
+
+torch.manual_seed(RANDOM_SEED)
+EPOCHS = 100
+
+X_blob_train, y_blob_train = X_blob_train.to(device), y_blob_train.to(device)
+X_blob_test, y_blob_test = X_blob_test.to(device), y_blob_test.to(device)
+
+for epoch in range(EPOCHS):
+
+    # train
+    model_4.train()
+
+    # forward pass
+    y_logits = model_4(X_blob_train)
+    y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+    # loss and accuracy
+    loss = loss_fn(y_logits, y_blob_train)
+    acc = torchmetrics_accuracy(y_pred, y_blob_train)
+
+    # optimizer grad zero
+    optimizer.zero_grad()
+
+    # backward pass
+    loss.backward()
+
+    # optimize weights
+    optimizer.step()
+
+    # testing
+    model_4.eval()
+    with torch.inference_mode():
+        test_logits = model_4(X_blob_test)
+        test_pred = torch.softmax(test_logits, dim=1).argmax(dim=1)
+
+        test_loss = loss_fn(test_logits, y_blob_test)
+        test_acc = torchmetrics_accuracy(test_pred, y_blob_test)
+
+    if epoch % 10 == 0:
+        print(
+            f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%")
+
+# make predictions
+model_4.eval()
+with torch.inference_mode():
+    y_logits = model_4(X_blob_test)
+
+y_preds = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model_4, X_blob_train, y_blob_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_4, X_blob_test, y_blob_test)
+plt.show()
